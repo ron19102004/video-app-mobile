2d38309831fb5bb4b7ad972c6659c9c1153bf818
ron19102004 <ron19102004@gmail.com>
2024-05-03 19:04:32 +0700

    update

--- app/src/main/java/com/video/app/ui/screens/components/PullToRefreshLazyColumn.kt
+++ app/src/main/java/com/video/app/ui/screens/components/PullToRefreshLazyColumn.kt
@@ -1,18 +1,31 @@
 package com.video.app.ui.screens.components
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.video.app.ui.theme.AppColor
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -50,10 +63,38 @@ fun <T> PullToRefreshLazyColumn(
                 pullToRefreshState.endRefresh()
             }
         }
-        if (isRefreshing)
+        if (pullToRefreshState.progress > 0.0f || isRefreshing)
             PullToRefreshContainer(
                 state = pullToRefreshState,
-                modifier = Modifier.align(Alignment.TopCenter)
+                modifier = Modifier.align(Alignment.TopCenter),
+                containerColor = AppColor.background_container,
+                contentColor = AppColor.primary_text
             )
     }
+}
+
+@Composable
+@Preview
+fun PullToRefreshLazyColumnPreview() {
+    var isRefreshing by remember {
+        mutableStateOf(false)
+    }
+    val scope = rememberCoroutineScope();
+    val reload: (action: () -> Unit) -> Unit = {
+        //Handle reload
+        it()
+    }
+    PullToRefreshLazyColumn<Any>(
+        isRefreshing = isRefreshing,
+        onRefresh = {
+            scope.launch {
+                isRefreshing = true
+                delay(1000L)
+                reload { isRefreshing = false }
+            }
+        },
+        contentFix = {
+            Text("Test")
+        }
+    )
 }
